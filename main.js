742fff3e9740fef69f37453bc25d08b59ad2e7c2
Dominguez <ashely.dominguez@accenture.com>
2024-09-24 14:23:25 -0400

    the fizz and fizzbuzz added to part 1 loop

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 //Part 1:Fizz Buzz
 
-for ( let i = 1; i <= 100; i++) {
-    console.log(i);
+let num = 100
 
-}
+for ( let i = 1; i <= num; i++) {
+    // console.log(i)
+} 
+
+
+for ( let i = 1; i <= num; i++) {
+    if (i % 3 == 0 ) {
+        console.log('Fizz')
+    } if (i % 5 == 0) {
+        console.log('Buzz')
+    } if ( i % 3 && i % 5 == 0) {
+        console.log('Fizz Buzz')
+    } if ( i % 3 !== 0 && i % 5 !== 0) {
+        console.log(i)
+    }
+} 
+
+
+
